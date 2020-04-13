ad006735108ecc043d39138d07e5402d82c1d916
eonist <30n1st@gmail.com>
2020-04-13 13:26:24 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "CommonCell",
+    platforms: [.iOS(.v12)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
