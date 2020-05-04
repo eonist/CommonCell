c637aef1af1f2f054876f7d9f395ab55296e086e
André J <30n1st@gmail.com>
2020-05-04 23:24:25 +0200

    Update ActionCell+Dequeue.swift

--- Sources/CommonCell/ActionCell/ActionCell+Dequeue.swift
+++ Sources/CommonCell/ActionCell/ActionCell+Dequeue.swift
@@ -6,6 +6,7 @@ import UIKit
 extension ActionCell {
    /**
     * Trick to deque based on Class.Type
+    * - Fixme: ⚠️️ move to basecell?
     */
    public static func dequeue(table: UITableView, indexPath: IndexPath) -> Self {
       table.dequeueReusableCell(indexPath: indexPath) as Self
