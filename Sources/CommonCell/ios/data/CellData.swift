f1323e6257d0105a50cdfa686ab6f97b34e4c5a9
eonist <30n1st@gmail.com>
2020-12-31 14:19:28 +0100

    Create CellData.swift

---
+++ Sources/CommonCell/ios/data/CellData.swift
@@ -0,0 +1,14 @@
+import UIKit
+import ReusableCell
+/**
+ * Container for data for title and value
+ * - Fixme: ⚠️️ Can be moved to different place
+ */
+public struct CellData: CellDataKind {
+   public let titleText: String
+   public let value: Any?
+   public init(titleText: String, value: Any?) {
+      self.titleText = titleText
+      self.value = value
+   }
+}
