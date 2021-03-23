710be4d8f6c08bb1d236c384127b047da96c7c8d
André J <30n1st@gmail.com>
2021-03-23 15:22:04 +0100

    Minor tweak

--- Sources/CommonCell/ios/data/CellData.swift
+++ Sources/CommonCell/ios/data/CellData.swift
@@ -7,7 +7,7 @@ import ReusableCell
 public struct CellData: CellDataKind {
    public let titleText: String
    public let value: Any?
-   public init(titleText: String, value: Any?) {
+   public init(titleText: String, value: Any? = nil) {
       self.titleText = titleText
       self.value = value
    }
