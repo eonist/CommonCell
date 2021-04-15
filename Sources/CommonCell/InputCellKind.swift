7addf98436f7456606e0ad06fac32ab1ede48c86
eonist <30n1st@gmail.com>
2021-04-16 01:00:31 +0200

    Add InputCellKinds

--- Sources/CommonCell/InputCellKind.swift
+++ Sources/CommonCell/InputCellKind.swift
@@ -4,3 +4,12 @@ public protocol InputCellKind {
    var inputText: String? { get }
    var titleText: String? { get }
 }
+public typealias InputCellKinds = [InputCellKind]
+extension InputCellKinds {
+   /**
+    * Find first for title
+    */
+   public func first(title: String) -> InputCellKind? {
+      self.first { $0.titleText == title }
+   }
+}
