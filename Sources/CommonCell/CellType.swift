b4dd17a868ac5a9d5a5d6b4b42f7e53cd2f6a4cd
André J <30n1st@gmail.com>
2021-09-13 15:02:09 +0200

    Add CellType

---
+++ Sources/CommonCell/CellType.swift
@@ -0,0 +1,32 @@
+import Foundation
+/**
+ * In this table: all Cells must have these attributes
+ * - Note: Used to store cell type and cell data in table models etc
+ */
+public struct CellType {
+   public let type: ActionCellKind.Type?
+   public let title: String
+   public let data: Any?
+   /**
+    * - Parameters:
+    *   - type: the cell type (We use celltype to reuse cell etc)
+    *   - title: title
+    *   - data: value
+    */
+   public init(type: ActionCellKind.Type?, title: String, data: Any? = nil) {
+      self.type = type
+      self.title = title
+      self.data = data
+   }
+}
+/**
+ * Getter
+ */
+extension CellType {
+   /**
+    * cellData
+    */
+   public var cellData: CellData {
+      .init(titleText: self.title, value: self.data)
+   }
+}
