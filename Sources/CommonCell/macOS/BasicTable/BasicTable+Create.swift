900b2d146153d337fc8f4967c55ef5f972637930
André J <30n1st@gmail.com>
2021-11-30 13:20:46 +0100

    Update BasicTable+Create.swift

--- Sources/CommonCell/macOS/BasicTable/BasicTable+Create.swift
+++ Sources/CommonCell/macOS/BasicTable/BasicTable+Create.swift
@@ -14,8 +14,9 @@ extension BasicTable {
       tableView.backgroundColor = .clear // .systemYellow // Remove background color
       tableView.intercellSpacing = .zero // Remove space between cells
       tableView.selectionHighlightStyle = .none // Avoid highlights on click etc
-      tableView.headerView = nil // No need for header, we want a vanilla table design
+      tableView.headerView = nil
       let column: NSTableColumn = .init(identifier: .init(Self.col1ID))
+//      column.headerCell = .init(textCell: "") // .init(Self.col1ID)
       column.width = .zero // default is 100px, but that may cause issues if table is less than 100 on init
       tableView.addTableColumn(column) // Generally you want to add at least one column to the table view.
       self.documentView = tableView // Set the tableview as the docView in the scrollview
