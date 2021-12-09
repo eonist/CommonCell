07a808d233208290826fd409727d60862dd53c7f
André J <30n1st@gmail.com>
2021-12-09 01:13:34 +0100

    Update lint

--- Sources/CommonCell/macOS/ActionCell/ActionCell+Const+Mac.swift
+++ Sources/CommonCell/macOS/ActionCell/ActionCell+Const+Mac.swift
@@ -5,7 +5,7 @@ import QuartzCore
  */
 extension ActionCell {
    public typealias OnAction = (_ actionCell: ActionCell, _ value: Any?) -> Void
-   public static let defaultOnAction: OnAction = { actionCell, value in Swift.print("defaultOnAction: \(String(describing: value))") }
+   public static let defaultOnAction: OnAction = { actionCell, value in Swift.print("defaultOnAction: \(String(describing: value)) actionCell: \(actionCell)") }
    @objc open class var height: CGFloat { 32 } // Standard size
 }
 #endif
