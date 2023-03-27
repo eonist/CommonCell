808da52c476d048156509f11d09848e758152d17
eonist <30n1st@gmail.com>
2023-03-27 03:58:56 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,18 +3,16 @@ import PackageDescription
 
 let package = Package(
     name: "CommonCell",
-    platforms: [.iOS(.v12), .macOS(.v10_13)],
+    platforms: [.iOS(.v12), .macOS(.v10_13)], // update to ios15 and update swift v to 5.6?
     products: [
         .library(
             name: "CommonCell",
             targets: ["CommonCell"])
     ],
-    dependencies: [
-        /*.package(url: "https://github.com/eonist/ReusableCell.git", .branch("master"))*/
-    ],
+    dependencies: [],
     targets: [
         .target(
             name: "CommonCell",
-            dependencies: [/*"ReusableCell"*/])
+            dependencies: [])
     ]
 )
