0bebe8899e11a1459e730363be9afbe25395e9fe
eonist <30n1st@gmail.com>
2023-04-18 13:55:26 +0200

    Minor tweak

--- Sources/CommonCell/InputCellType.swift
+++ Sources/CommonCell/InputCellType.swift
@@ -1,3 +1,4 @@
 import Foundation
-
+// ⚠️️ deprecated
+@available(*, deprecated, renamed: "CellType")
 public typealias InputCellType = CellType
--- Sources/CommonCell/iOS/SectionData.swift
+++ Sources/CommonCell/iOS/SectionData.swift
@@ -3,5 +3,5 @@ import Foundation
 /**
  * Used to store sections with rows (table model)
  */
-public typealias SectionData = (title: String?, rows: [InputCellType])
+public typealias SectionData = (title: String?, rows: [CellType])
 #endif
