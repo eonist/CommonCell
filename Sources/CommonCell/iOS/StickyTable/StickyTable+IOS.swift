b78ed0ea456cbd40571e8cb625d728c95368daee
eonist <30n1st@gmail.com>
2023-07-13 19:50:06 +0200

    MInor tweaks

--- Sources/CommonCell/iOS/StickyTable/StickyTable+IOS.swift
+++ Sources/CommonCell/iOS/StickyTable/StickyTable+IOS.swift
@@ -11,8 +11,8 @@ open class StickyTable: BaseTable {
    /**
     * This table is only cell based, it doesn't use sections. Because we want to use the same structure for macOS, and macOS doesn't have sections
     * - Parameters:
-    *   - frame: - Fixme: ⚠️️ add doc
-    *   - style: - Fixme: ⚠️️ add doc
+    *   - frame: size of table
+    *   - style: plain or grouped
     */
    override public init(frame: CGRect = .zero, style: UITableView.Style = .plain) {
       super.init(frame: frame, style: style)
